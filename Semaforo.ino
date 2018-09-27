db7d1e7bb914e2c1d211a9c5b5d3df9b7ba04b74
feffe93 <feffe93@gmail.com>
2018-09-27 13:17:23 +0200

    File
    Eccolo

---
+++ Semaforo.ino
@@ -0,0 +1,22 @@
+//9 HIGH rosso acceso
+//10 HIGH verde acceso (con 9 LOW)
+//10 LOW arancione acceso (con 9 LOW)
+void setup() 
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() 
+{
+   
+   digitalWrite(9, HIGH);
+   delay(4000);
+   digitalWrite(9, LOW);
+   digitalWrite(10, HIGH);   
+   delay(4000);
+   digitalWrite(10, LOW);
+   delay(2000);
+   
+  
+}
